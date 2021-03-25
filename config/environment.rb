da29291b99866286aee4ad9c0666c6f982ddc686
remote-torrent-6747 <abbyc@sky.com>
2021-03-25 20:12:24 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,11 @@ Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| requi
 
 ENV["SCHOOL_ENV"] ||= "development"
 
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/students.sqlite"
+)
+
 DBRegistry[ENV["SCHOOL_ENV"]].connect!
 DB = ActiveRecord::Base.connection
 
@@ -18,8 +23,3 @@ def drop_db
     DB.execute("DROP TABLE #{table}")
   end
 end
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/students.sqlite"
-)
